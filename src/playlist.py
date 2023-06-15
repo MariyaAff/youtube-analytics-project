ebff4717944800b03b0d8e704cca66727f3acbed
Мария Юркшайтене <masha.afan@bk.ru>
2023-06-15 16:57:35 +0300

    Homework5

---
+++ src/playlist.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+class PlayList:
+    def __init__(self, id_play, name_list, url_list):
+        self.id_play = id_play
+        self.name_list = name_list
+        self.url_list = url_list
+
+    def total_duration(self):
+        pass
+
+    def show_best_video(self):
+        pass
+    
